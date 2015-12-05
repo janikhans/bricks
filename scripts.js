e2508233ef8859e97b3ee10c53103ba6c48551b8
Janik Knittle <janik.knittle@gmail.com>
2015-12-04 17:09:59 -0800

    More formatting to scripts file and a little refactoring. Move speeds up now when hit

--- scripts.js
+++ scripts.js
@@ -48,155 +48,155 @@ document.addEventListener("mousemove", mouseMoveHandler, false);
 //an array to hold all the bricks and their locations
 var bricks = [];
 for ( c = 0; c < brickColumnCount; c++) {
-bricks[c] = [];
-for (r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = { x:0, y: 0, status: 1};
-}
+    bricks[c] = [];
+    for (r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x:0, y: 0, status: 1};
+    }
 }
 
 //like the name implies, this function will draw out each brick to the canvas.
 function drawBricks() {
-for ( c = 0; c < brickColumnCount; c++) {
-    for ( r = 0; r < brickRowCount; r++) {
-        if ( bricks[c][r].status == 1) {
-            var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
-            var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+    for ( c = 0; c < brickColumnCount; c++) {
+        for ( r = 0; r < brickRowCount; r++) {
+            if ( bricks[c][r].status == 1) {
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
-}
 }
 
 //ball code, size color etc.
 function drawBall() {
-ctx.beginPath();
-ctx.arc(x,y,ballRadius,0, Math.PI*2);
-ctx.fillStyle = ballColor;
-ctx.fill();
-ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(x,y,ballRadius,0, Math.PI*2);
+    ctx.fillStyle = ballColor;
+    ctx.fill();
+    ctx.closePath();
 }
 
 //should be obvious.
 function drawPaddle() {
-ctx.beginPath();
-ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-ctx.fillStyle = '#0095DD';
-ctx.fill();
-ctx.closePath();
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
 }
 
 function drawScore() {
-ctx.font = "16px Arial";
-ctx.fillStyle = "#0095DD";
-ctx.fillText("Score: " + score, 8, 20);
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: " + score, 8, 20);
 }
 
 function drawLives() {
-ctx.font = "16px Arial";
-ctx.fillstyle = "#0095DD";
-ctx.fillText("Lives " + lives, canvas.width-65, 20);
+    ctx.font = "16px Arial";
+    ctx.fillstyle = "#0095DD";
+    ctx.fillText("Lives " + lives, canvas.width-65, 20);
 }
 
 
 function ranBallColor() {
-ballColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    ballColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 }
 
 //the handlers for the keystrokes
 function keyDownHandler(e) {
-if (e.keyCode == 39) {
-    rightPressed = true;
-} else if (e.keyCode == 37) {
-    leftPressed = true;
-}
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    } else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
 }
 
 function keyUpHandler(e) {
-if (e.keyCode == 39) {
-    rightPressed = false;
-} else if (e.keyCode == 37) {
-    leftPressed = false;
-}
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    } else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
 }
 
 //handler for mouse movements
 function mouseMoveHandler(e) {
-var relativeX = e.clientX - canvas.offsetLeft;
-if(relativeX > 0 && relativeX < canvas.width) {
-    paddleX = relativeX - paddleWidth/2;
-}
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
 }
 
 function collisionDetection() {
-for(c=0; c<brickColumnCount; c++) {
-    for(r=0; r<brickRowCount; r++) {
-        var b = bricks[c][r];
-        if(b.status == 1) {
-            if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-                dy = -dy;
-                b.status = 0;
-                ranBallColor();
-                score++;
-                if ( score == brickColumnCount*brickRowCount) {
-                    alert("You win this awesome game!");
-                    document.location.reload();
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    ranBallColor();
+                    score++;
+                    if ( score == brickColumnCount*brickRowCount) {
+                        alert("You win this awesome game!");
+                        document.location.reload();
+                    }
                 }
             }
         }
     }
 }
-}
 
 
 //This will draw to our canvas
 function draw() {
-ctx.clearRect(0,0, canvas.width, canvas.height);
-drawBricks();
-drawBall();
-drawPaddle();
-drawScore();
-drawLives();
-collisionDetection();
-x += dx;
-y += dy;
-//Reverse the ball if it finds the edges of the canvas
-if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-}
-if (y + dy < ballRadius) {
-    dy = -dy;
-} else if (y + dy > canvas.height-ballRadius ) {
-    if (x > paddleX && x < paddleX + paddleWidth) {
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawLives();
+    collisionDetection();
+    x += dx;
+    y += dy;
+    //Reverse the ball if it finds the edges of the canvas
+    if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy < ballRadius) {
         dy = -dy;
-//                dy += -1;
-//                dx += 1;
-    } else {
-        lives--;
-        if (!lives) {
-            alert("GAME OVER! You scored " + score + " points.");
-            document.location.reload();
+    } else if (y + dy > canvas.height-ballRadius ) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+            dy += -1;
+            dx += 1;
         } else {
-            x = canvas.width/2;
-            y = canvas.height-30;
-            dx = 2;
-            dy = -2;
-            paddleX = (canvas.width-paddleWidth)/2;
+            lives--;
+            if (!lives) {
+                alert("GAME OVER! You scored " + score + " points.");
+                document.location.reload();
+            } else {
+                x = canvas.width/2;
+                y = canvas.height-30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width-paddleWidth)/2;
+            }
         }
     }
-}
-if (rightPressed && paddleX < canvas.width-paddleWidth) {
-    paddleX += 7;
-}
-if (leftPressed && paddleX > 0) {
-    paddleX += -7;
-}
-requestAnimationFrame(draw);
+    if (rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    if (leftPressed && paddleX > 0) {
+        paddleX += -7;
+    }
+    requestAnimationFrame(draw);
 }
 
 draw();
